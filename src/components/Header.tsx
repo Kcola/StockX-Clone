b043154931a09a9d4d1ad4c9787c7bac219dd531
Kolawole Campbell <kolawole.campbell@gmail.com>
2020-08-13 22:43:54 -0400

    added nav items.

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -12,10 +12,48 @@ const Logo = styled.img`
   padding: 60px;
   zoom: 0.4;
 `;
+const Nav = styled.ul`
+  list-style-type: none;
+  display: flex;
+`;
+const NavItem = styled.li`
+  margin: 5px;
+  font-size: 0.8rem;
+  display: flex;
+  align-items: center;
+`;
+const NavLink = styled.a`
+  color: black;
+  text-decoration: none;
+`;
+class NavInfo {
+  text: string;
+  href: string;
+  constructor(text: string, href: string) {
+    this.text = text;
+    this.href = href;
+  }
+}
+const navList = [
+  new NavInfo("Browse", "#"),
+  new NavInfo("News", "#"),
+  new NavInfo("App", "#"),
+  new NavInfo("About", "#"),
+  new NavInfo("Help", "#"),
+  new NavInfo("Login", "#"),
+  new NavInfo("Sign Up", "#"),
+];
 function Header() {
   return (
     <HeaderContainer>
       <Logo src={logoDark} />
+      <Nav>
+        {navList.map((x) => (
+          <NavItem>
+            <NavLink href={x.href}>{x.text}</NavLink>
+          </NavItem>
+        ))}
+      </Nav>
     </HeaderContainer>
   );
 }
